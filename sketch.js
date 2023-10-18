5774702de353b780b3d779a67ea5d48afc1bcd58
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-18 03:43:15 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,4 +37,52 @@ function draw() {
 
 class Particle {
     constructor() {
-        this.pos = createVector(random(-width / 2, width / 2), random(-height / 2, height / 2), random(-
+        this.pos = createVector(random(-width / 2, width / 2), random(-height / 2, height / 2), random(-200, 200));
+        this.vel = p5.Vector.random3D().mult(random(2, 4)); // Random initial velocity
+        this.size = random(5, 15); // Random size
+        this.isStriking = false;
+    }
+
+    update() {
+        this.pos.add(this.vel);
+    }
+
+    display() {
+        if (this.isStriking) {
+            fill(255, 0, 0); // Red when striking
+        } else {
+            noFill();
+        }
+        stroke(255);
+        strokeWeight(2);
+        push();
+        translate(this.pos.x, this.pos.y, this.pos.z);
+        sphere(this.size);
+        pop();
+    }
+
+    checkBounds() {
+        if (
+            this.pos.x > width / 2 ||
+            this.pos.x < -width / 2 ||
+            this.pos.y > height / 2 ||
+            this.pos.y < -height / 2 ||
+            this.pos.z > 200 ||
+            this.pos.z < -200
+        ) {
+            this.vel.mult(-1); // Bounce off the walls
+        }
+    }
+
+    checkCollision(otherParticles) {
+        for (let other of otherParticles) {
+            if (other !== this) {
+                let d = dist(this.pos.x, this.pos.y, this.pos.z, other.pos.x, other.pos.y, other.pos.z);
+                if (d <= (this.size / 2 + other.size / 2)) {
+                    this.isStriking = true; // Change color when striking another particle
+                    other.isStriking = true;
+                }
+            }
+        }
+    }
+}
