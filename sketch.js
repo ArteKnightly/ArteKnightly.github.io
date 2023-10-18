33a349a141bf46f3406867ddeda533c44e883509
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-18 03:21:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,9 +17,9 @@ function draw() {
 
     // Draw and update particles
     for (let p of particles) {
-        p.followMouse(); // Make particles follow the mouse closely
         p.update();
         p.display();
+        p.checkCollision(); // Check for collision with the cube
     }
 
     // Draw a rotating cube in the center
@@ -35,27 +35,43 @@ function draw() {
 
 class Particle {
     constructor() {
-        this.pos = createVector(random(-width, width), random(-height, height), random(-200, 200));
-        this.vel = p5.Vector.random3D();
-    }
-
-    followMouse() {
-        let target = createVector(mouseX - width / 2, mouseY - height / 2);
-        let dir = p5.Vector.sub(target, this.pos);
-        dir.normalize();
-        this.vel.add(dir.mult(0.5)); // Make particles follow the mouse closely
+        this.pos = createVector(random(-width / 2, width / 2), random(-height / 2, height / 2), random(-200, 200));
+        this.vel = p5.Vector.random3D().mult(random(2, 4)); // Random initial velocity
+        this.size = random(5, 15); // Random size
     }
 
     update() {
         this.pos.add(this.vel);
-        if (this.pos.x > width || this.pos.x < -width || this.pos.y > height || this.pos.y < -height || this.pos.z > 200 || this.pos.z < -200) {
-            this.pos = createVector(random(-width, width), random(-height, height), random(-200, 200));
-        }
+        this.checkBounds(); // Check if the particle is outside the box
     }
 
     display() {
         stroke(255);
         strokeWeight(2);
-        point(this.pos.x, this.pos.y, this.pos.z);
+        push();
+        translate(this.pos.x, this.pos.y, this.pos.z);
+        sphere(this.size); // You can use different shapes like box, sphere, etc.
+        pop();
+    }
+
+    checkBounds() {
+        if (this.pos.x > width / 2 || this.pos.x < -width / 2) {
+            this.vel.x *= -1; // Bounce off the walls
+        }
+        if (this.pos.y > height / 2 || this.pos.y < -height / 2) {
+            this.vel.y *= -1; // Bounce off the ceiling and floor
+        }
+        if (this.pos.z > 200 || this.pos.z < -200) {
+            this.vel.z *= -1; // Bounce off the front and back of the box
+        }
+    }
+
+    checkCollision() {
+        // Check for collision with the cube
+        let d = dist(0, 0, 0, this.pos.x, this.pos.y, this.pos.z);
+        if (d <= 50) {
+            let normal = createVector(0, 0, 1); // Normal vector of the cube's front face
+            this.vel.reflect(normal); // Reflect velocity off the cube
+        }
     }
 }
