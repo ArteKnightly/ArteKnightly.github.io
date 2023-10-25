ec9b9cd3bb7c28c91e0dfdbb033a0cf0b3cee1b5
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-25 02:38:27 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,133 +1,157 @@
-// sketch.js
 let particles = [];
-let angle = 0;
 let cubeSize = 100;
-let trappedParticles = [];
+let cubeRotationX = 0;
+let cubeRotationY = 0;
+let burstParticles = [];
+let burstTime = 5000; // 5 seconds
+let startTime;
+let cubeColor = 200; // Fixed cube color
+
+// Bounce direction variables
+let xDirection = 1;
+let yDirection = 1;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL); // Create a 3D canvas
-    stroke(255);
-    for (let i = 0; i < 50; i++) {
-        particles.push(new StarParticle());
-    }
+  createCanvas(windowWidth, windowHeight, WEBGL); // Use the WEBGL renderer
+  noStroke();
+  startTime = millis();
+
+  // Initialize particles in random positions
+  for (let i = 0; i < 20; i++) {
+    particles.push(new Particle(random(-width / 2, width / 2), random(-height / 2, height / 2), 5));
+  }
 }
 
 function draw() {
-    background(0);
-    rotateY(angle); // Rotate the scene
+  background(0);
+  let dampingFactor = 0.8; // Adjust this value as needed
+  // Inside the draw function
+  for (let p of particles) {
+  p.update(mouseX - width / 2, mouseY - height / 2, dampingFactor);
+  p.display();
+  }
+  // Rotate the cube
+  cubeRotationX += 0.01 * xDirection;
+  cubeRotationY += 0.01 * yDirection;
 
-    // Draw the wireframe cube in the center
-    push();
-    translate(0, 0, -200); // Position the cube in front of the particles
-    rotateX(angle * 0.5);
-    rotateY(angle * 0.5);
-    noFill();
-    box(cubeSize);
-    pop();
+  // Bounce when the cube reaches a certain angle
+  if (cubeRotationX > PI / 4 || cubeRotationX < -PI / 4) {
+    xDirection *= -1;
+  }
+  if (cubeRotationY > PI / 4 || cubeRotationY < -PI / 4) {
+    yDirection *= -1;
+  }
 
-    // Draw and update particles
-    for (let i = particles.length - 1; i >= 0; i--) {
-        let p = particles[i];
-        p.update();
-        p.display();
-        p.checkBounds();
+  push();
+  translate(0, 0, 0);
+  rotateX(cubeRotationX);
+  rotateY(cubeRotationY);
+  noFill(); // Wireframe cube
+  stroke(cubeColor);
+  box(cubeSize);
+  pop();
 
-        // Check for collision with the cube
-        if (p instanceof StarParticle && p.checkCollision(cubeSize)) {
-            trappedParticles.push(p);
-            particles.splice(i, 1);
+  // Remove particles that hit the cube
+  for (let i = particles.length - 1; i >= 0; i--) {
+    let p = particles[i];
+    p.update(mouseX - width / 2, mouseY - height / 2);
+    p.display();
+
+    // Check for collision with the cube
+    if (p.checkCollision(cubeSize)) {
+      burstParticles.push(...p.burst());
+      particles.splice(i, 2);
+    }
+  }
+
+  // Display the burst particles
+  for (let p of burstParticles) {
+    p.update();
+    p.display();
+  }
+
+  // End animation when all particles are gone
+  if (particles.length === 0) {
+    if (millis() - startTime < burstTime) {
+      textSize(64);
+      fill(255);
+      text("ENTER", -50, 0);
+    } else {
+      // Burst into tiny particles
+      if (burstParticles.length === 0) {
+        for (let i = 0; i < 500; i++) {
+          burstParticles.push(new Particle(0, 0, random(2, 5)));
         }
+      }
     }
-
-    // Draw trapped particles
-    for (let p of trappedParticles) {
-        p.display();
-    }
-
-    angle += 0.01; // Increment rotation angle for cube and scene
+  }
 }
 
 class Particle {
-    constructor() {
-        this.pos = createVector(random(-width / 2, width / 2), random(-height / 2, height / 2), random(-200, 200));
-        this.vel = p5.Vector.random3D().mult(random(2, 4)); // Random initial velocity
-        this.size = random(5, 15); // Random size
+  constructor(x, y, speed) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.size = 10;
+  }
+  repel(other, minDistance, maxForce) {
+    let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+    if (d > 0 && d < minDistance) {
+      let force = p5.Vector.sub(this.pos, other.pos);
+      force.normalize();
+      force.mult(map(d, 0, minDistance, maxForce, 0));
+      this.acc.add(force);
+    }
+  }
+
+  update(targetX, targetY, damping) {
+    let target = createVector(targetX, targetY);
+    let dir = p5.Vector.sub(target, this.pos);
+    dir.setMag(.5); // Set the speed to a constant value (e.g., 5)
+
+    // Apply damping
+    this.vel.mult(damping);
+
+    this.acc = dir;
+
+    // Calculate repulsion force between particles
+    for (let other of particles) {
+      if (other !== this) {
+        this.repel(other, Math.floor(Math.random() * (50 - 15 + 1)) + 15, 6); // Adjust minDistance and maxForce as needed
+      }
     }
 
-    update() {
-        this.pos.add(this.vel);
-    }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
 
-    display() {
-        stroke(255, 0, 0); // Red outline
-        strokeWeight(2);
-        push();
-        translate(this.pos.x, this.pos.y, this.pos.z);
-        // Specific particle rendering goes here
-        pop();
+    // Check boundaries
+    if (this.pos.x < -width / 2 || this.pos.x > width / 2) {
+      this.vel.x *= -1; // Reverse the x-velocity to bounce off horizontally
     }
+    if (this.pos.y < -height / 2 || this.pos.y > height / 2) {
+      this.vel.y *= -1; // Reverse the y-velocity to bounce off vertically
+    }
+  }
 
-    checkBounds() {
-        if (
-            this.pos.x > width / 2 ||
-            this.pos.x < -width / 2 ||
-            this.pos.y > height / 2 ||
-            this.pos.y < -height / 2 ||
-            this.pos.z > 200 ||
-            this.pos.z < -200
-        ) {
-            this.vel.mult(-1); // Bounce off the walls
-        }
-    }
+  display() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, this.size, this.size);
+  }
 
-    checkCollision(cubeSize) {
-        // Specific collision checking goes here
-        return false;
-    }
-}
+  checkCollision(cubeSize) {
+    return (
+      this.pos.x > -cubeSize / 2 &&
+      this.pos.x < cubeSize / 2 &&
+      this.pos.y > -cubeSize / 2 &&
+      this.pos.y < cubeSize / 2
+    );
+  }
 
-class StarParticle extends Particle {
-    constructor() {
-        super();
-        this.isStar = true;
-        this.isStriking = false;
-        this.tracer = [];
+  burst() {
+    const newParticles = [];
+    for (let i = 0; i < 10; i++) {
+      newParticles.push(new Particle(this.pos.x, this.pos.y, random(1, 3)));
     }
-
-    update() {
-        super.update();
-        this.tracer.push(createVector(this.pos.x, this.pos.y, this.pos.z));
-        if (this.tracer.length > 20) {
-            this.tracer.shift();
-        }
-    }
-
-    display() {
-        super.display();
-        noFill();
-        for (let i = 0; i < this.tracer.length - 1; i++) {
-            let alpha = map(i, 0, this.tracer.length - 1, 100, 0);
-            stroke(255, 0, 0, alpha);
-            line(this.tracer[i].x, this.tracer[i].y, this.tracer[i].z, this.tracer[i + 1].x, this.tracer[i + 1].y, this.tracer[i + 1].z);
-        }
-        push();
-        translate(this.pos.x, this.pos.y, this.pos.z);
-        // Render the star
-        pop();
-    }
-
-    checkCollision(cubeSize) {
-        // Check if the star is inside the cube
-        if (
-            this.pos.x + this.size / 2 < cubeSize / 2 &&
-            this.pos.x - this.size / 2 > -cubeSize / 2 &&
-            this.pos.y + this.size / 2 < cubeSize / 2 &&
-            this.pos.y - this.size / 2 > -cubeSize / 2 &&
-            this.pos.z + this.size / 2 < cubeSize / 2 &&
-            this.pos.z - this.size / 2 > -cubeSize / 2
-        ) {
-            return true;
-        }
-        return false;
-    }
+    return newParticles;
+  }
 }
