0811ed062dd2183c8421584c3948fddb425c33aa
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-30 17:28:24 -0400

    Update Critique.js

--- Critique.js
+++ Critique.js
@@ -39,6 +39,44 @@ function setup() {
     // Default selected image
     currentImageIndex = 0;
 }
+function draw() {
+    background(0);  // Black background
+
+    // Display the spinning object at the center top
+    currentShapeObj.display(0);
+
+    if (images.length > 0) {
+        let img = images[currentImageIndex].img;
+
+        // Define padding values
+        let leftPad = 20;
+        let rightPad = 20;
+        let bottomPad = 50;
+
+        // Calculate maximum display dimensions
+        let maxDisplayWidth = width - leftPad - rightPad;
+        let maxDisplayHeight = height - (topPad + currentShapeObj.size) - bottomPad;
+
+        // Calculate the aspect ratio of the image
+        let imgAspectRatio = img.width / img.height;
+
+        // Calculate display width and height based on aspect ratio
+        let displayWidth = maxDisplayWidth;
+        let displayHeight = displayWidth / imgAspectRatio;
+
+        if (displayHeight > maxDisplayHeight) {
+            displayHeight = maxDisplayHeight;
+            displayWidth = displayHeight * imgAspectRatio;
+        }
+
+        // Calculate image position
+        let imageXPosition = width / 2; // Centered in x-axis
+        let imageYPosition = topPad + currentShapeObj.size;
+
+        // Display the image
+        image(img, imageXPosition - displayWidth / 2, imageYPosition, displayWidth, displayHeight);
+    }
+}
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
