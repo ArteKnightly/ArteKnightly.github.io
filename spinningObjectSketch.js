4d0a678efcc77276b2f9ba97ff38bfaa7f724b2c
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-30 14:36:22 -0400

    Update spinningObjectSketch.js

--- spinningObjectSketch.js
+++ spinningObjectSketch.js
@@ -19,53 +19,52 @@ class ObjShape {
         this.noiseOffsetY = random(1001, 2000);  // For Perlin noise
     }
 
-    display() {
-        // Determine fill color based on posX
-        let normalizedX = map(this.posX, -width / 2, width / 2, -1, 1);
-        
-        normalizedX = constrain(normalizedX, -1, 1);
+display(yTranslate = 0) {
+    // Determine fill color based on posX
+    let normalizedX = map(this.posX, -width / 2, width / 2, -1, 1);
+    normalizedX = constrain(normalizedX, -1, 1);
 
-        if (normalizedX < 0) {
-            // Transition to turquoise
-            let col = lerpColor(color(0, 0, 0, 0), color(64, 224, 208, 255), abs(normalizedX)); // Turquoise
-            fill(col);
-        } else {
-            // Transition to cadmium red light
-            let col = lerpColor(color(0, 0, 0, 0), color(255, 48, 48, 255), normalizedX); // Cadmium Red Light
-            fill(col);
-        }
-
-        // Interpolate outline from white to black
-        let outlineCol = lerpColor(color(255), color(0), abs(normalizedX));
-        stroke(outlineCol);
-        display(yTranslate = 0) {
-        push();
-        translate(this.posX, yTranslate, 0);
-        rotateX(frameCount * this.spin + noise(this.noiseOffsetX) * 0.1);  // Incorporate noise into spin
-        rotateY(frameCount * this.rotate + noise(this.noiseOffsetY) * 0.1);  // Incorporate noise into rotate
-
-        if (this.type === 'box') {
-            box(this.size, this.size, this.size);
-        } else if (this.type === 'sphere') {
-            sphere(this.size, this.res.detailX, this.res.detailY);
-        } else if (this.type === 'cylinder') {
-            cylinder(this.size, this.size, this.res.detailX, this.res.detailY);
-        } else if (this.type === 'cone') {
-            cone(this.size, this.size, this.res.detailX, this.res.detailY);
-        } else if (this.type === 'torus') {
-            torus(this.size, this.size / 2, this.res.detailX, this.res.detailY);  // Size divided by 2 for inner radius
-        }
-        pop();
-        }
-
-        this.noiseOffsetX += 0.001;  // Adjust as needed
-        this.noiseOffsetY += 0.001;  // Adjust as needed
+    if (normalizedX < 0) {
+        // Transition to turquoise
+        let col = lerpColor(color(0, 0, 0, 0), color(64, 224, 208, 255), abs(normalizedX)); // Turquoise
+        fill(col);
+    } else {
+        // Transition to cadmium red light
+        let col = lerpColor(color(0, 0, 0, 0), color(255, 48, 48, 255), normalizedX); // Cadmium Red Light
+        fill(col);
     }
 
-    setPosition(x) {
-        this.posX = x;
+    // Interpolate outline from white to black
+    let outlineCol = lerpColor(color(255), color(0), abs(normalizedX));
+    stroke(outlineCol);
+    
+    // Render object with translation
+    push();
+    translate(this.posX, yTranslate, 0);
+    rotateX(frameCount * this.spin + noise(this.noiseOffsetX) * 0.1);  // Incorporate noise into spin
+    rotateY(frameCount * this.rotate + noise(this.noiseOffsetY) * 0.1);  // Incorporate noise into rotate
+
+    if (this.type === 'box') {
+        box(this.size, this.size, this.size);
+    } else if (this.type === 'sphere') {
+        sphere(this.size, this.res.detailX, this.res.detailY);
+    } else if (this.type === 'cylinder') {
+        cylinder(this.size, this.size, this.res.detailX, this.res.detailY);
+    } else if (this.type === 'cone') {
+        cone(this.size, this.size, this.res.detailX, this.res.detailY);
+    } else if (this.type === 'torus') {
+        torus(this.size, this.size / 2, this.res.detailX, this.res.detailY);  // Size divided by 2 for inner radius
     }
+    pop();
+
+    this.noiseOffsetX += 0.001;  // Adjust as needed
+    this.noiseOffsetY += 0.001;  // Adjust as needed
 }
+
+setPosition(x) {
+    this.posX = x;
+}
+
 
 function setup() {
     switchShape('box');  // Start with the box shape by default
