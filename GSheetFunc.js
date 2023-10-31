9e523c6fc6475d55af87ede37b627e4efeab0c10
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-30 22:57:20 -0400

    Create GSheetFunc.js

---
+++ GSheetFunc.js
@@ -0,0 +1,31 @@
+function sheetRead(){
+httpDo(
+  'YOUR_GOOGLE_SCRIPT_URL?sheetName=ImageManifest',
+  'GET',
+  function(res) {
+    console.log(res);
+    // Process the returned data here
+  }
+);
+};
+
+functionSheetWrite(){
+let data = {
+  sheetName: 'ImageManifest',
+  UUIDImage: 'some_uuid',
+  ImageName: 'sample_image',
+  //... other fields ...
+};
+
+httpDo(
+  'YOUR_GOOGLE_SCRIPT_URL',
+  {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: data
+  },
+  function(res) {
+    console.log(res);
+  }
+);
+};
