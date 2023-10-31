df485ee778c6723e0dc934e90b8a50afa0645d29
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-31 10:55:59 -0400

    Update Critique.js

--- Critique.js
+++ Critique.js
@@ -84,7 +84,7 @@ function calculateImageAspectRatio(img) {
 }
 
 function imageXPos() {
-    return leftPad - displayImageWidth(getImg()) / 2;
+    return displayImageWidth(getImg()) / 2;
 }
 
 function imageYPos() {
