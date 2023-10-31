ffc7e7fc3b5a171d002176a5b95f73382dd87789
Arte Knightly <148173724+ArteKnightly@users.noreply.github.com>
2023-10-30 23:03:29 -0400

    Update GSheetFunc.js

--- GSheetFunc.js
+++ GSheetFunc.js
@@ -1,31 +1,33 @@
-function sheetRead(){
-httpDo(
-  'YOUR_GOOGLE_SCRIPT_URL?sheetName=ImageManifest',
-  'GET',
-  function(res) {
-    console.log(res);
-    // Process the returned data here
-  }
-);
-};
+// GSheetFunc.js
 
-functionSheetWrite(){
-let data = {
-  sheetName: 'ImageManifest',
-  UUIDImage: 'some_uuid',
-  ImageName: 'sample_image',
-  //... other fields ...
-};
+export function sheetRead() {
+    httpDo(
+        'YOUR_GOOGLE_SCRIPT_URL?sheetName=ImageManifest',
+        'GET',
+        function(res) {
+            console.log(res);
+            // Process the returned data here
+        }
+    );
+}
 
-httpDo(
-  'YOUR_GOOGLE_SCRIPT_URL',
-  {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: data
-  },
-  function(res) {
-    console.log(res);
-  }
-);
-};
+export function sheetWrite() {
+    let data = {
+        sheetName: 'ImageManifest',
+        UUIDImage: 'some_uuid',
+        ImageName: 'sample_image',
+        //... other fields ...
+    };
+
+    httpDo(
+        'YOUR_GOOGLE_SCRIPT_URL',
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: data
+        },
+        function(res) {
+            console.log(res);
+        }
+    );
+}
