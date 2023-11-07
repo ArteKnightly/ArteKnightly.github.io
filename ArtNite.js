e155d6410c97a4aa17e0e8ffaba1c403eb45a9e9
ArteKnightly <ArteKnightley@gmail.com>
2023-11-07 13:24:08 -0500

    x

--- ArtNite.js
+++ ArtNite.js
@@ -26,7 +26,7 @@ function setup() {
     
     background(0); 
     drawGridFrame();
-    displayLatestEventDetails();
+    displayLatestEventDetails("setup");
     
   
 }
@@ -43,28 +43,30 @@ function initializeHues() {
     startHue = 360;  
     endHue = 0;     
 }
-function displayLatestEventDetails() {
+function displayLatestEventDetails(_mode) {
     textAlign(CENTER, CENTER); // Center the text both horizontally and vertically
 
     // Find the event with the largest UUIDEvent
     let latestEvent = jsonData.artNite.reduce((prev, current) => (prev.UUIDEvent > current.UUIDEvent) ? prev : current);
-  
+
     // Display the details
-    
     textSize(h1);
     text(`Art Nite?`, width / 2, paddingTop + h1 + spaceBetween);
     text(`${latestEvent.Date}`, width / 2, paddingTop + h1 + spaceBetween * 2 + h2);
     textSize(h2);
     text('Location: the Compound', width / 2, paddingTop + h1 + spaceBetween * 3 + h2 * 2);
 
-    // Create hyperlink to SpotifyEdit
-    let contributeLink = createA(latestEvent.SpotifyEdit, 'Contribute to playlist', '_blank');
-    contributeLink.position(width / 2 - contributeLink.width / 2, paddingTop + h1 + spaceBetween * 4 + h2 * 3); 
+    if (_mode === 'setup') { // Correct the conditional check
+        // Create hyperlink to SpotifyEdit
+        let contributeLink = createA(latestEvent.SpotifyEdit, 'Contribute to playlist', '_blank');
+        contributeLink.position(width / 2 - contributeLink.width / 2, paddingTop + h1 + spaceBetween * 4 + h2 * 3);
 
-    // Embed Spotify player
-    let spotifyEmbed = createElement('div', latestEvent.SpotifyembedIframe);
-    spotifyEmbed.position(width / 2 - 150, paddingTop + h1 + spaceBetween * 5 + h2 * 4); 
+        // Embed Spotify player
+        let spotifyEmbed = createElement('div', latestEvent.SpotifyembedIframe);
+        spotifyEmbed.position(width / 2 - 150, paddingTop + h1 + spaceBetween * 5 + h2 * 4);
+    }
 }
+
 
 function drawGridFrame() {
     let gridWidth = spaceBetween;
@@ -129,14 +131,14 @@ function drawCell(x, y, xOffset, yOffset, gridWidth, gridHeight) {
 }
 
 function draw() {
-    let transparency = 10; // Set transparency from 0 (fully transparent) to 255 (fully opaque)
+    let transparency = 1; // Set transparency from 0 (fully transparent) to 255 (fully opaque)
     background(0, 0, 0, transparency);
     drawGridFrame();
     xStart += incrementX;
     yStart += incrementY;
     zStart += incrementZ;
     let noiseValue = noise((xStart)*100);
-    displayLatestEventDetails();
+    displayLatestEventDetails("draw");
     if (mouseIsPressed) {
         stroke(255);
         strokeWeight(map(noiseValue, 0, 1,5, 20));
