42a0b4638a50fd512c201a7b0e2a86f6183a5a0f
ArteKnightly <ArteKnightley@gmail.com>
2023-11-09 04:14:04 -0500

    updates

--- readyImagesForWeb.py
+++ readyImagesForWeb.py
@@ -4,19 +4,21 @@ import json
 import shutil
 import uuid
 from datetime import datetime
-from generateManifest import generateManifest
 from imageProcess import process_image
-import random
-import string
 
 raw_directory = "raw"
 completed_directory = os.path.join(raw_directory, "complete")
 image_directory = "images"
-manifest_path = os.path.join(image_directory, 'manifest.json')
+data_directory = "data"
+manifest_path = os.path.join(data_directory, 'manifest.json')
 
-# Ensure completed_directory exists
+# Ensure directories exist
 if not os.path.exists(completed_directory):
     os.makedirs(completed_directory)
+if not os.path.exists(data_directory):
+    os.makedirs(data_directory)
+if not os.path.exists(image_directory):
+    os.makedirs(image_directory)
 
 # Load the existing manifest or create an empty one
 if os.path.exists(manifest_path):
@@ -24,20 +26,17 @@ if os.path.exists(manifest_path):
         manifest_data = json.load(f)
 else:
     manifest_data = {"images": []}
-# ...
 
 # Process each image in the raw directory
 for image_filename in os.listdir(raw_directory):
     if image_filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         input_path = os.path.join(raw_directory, image_filename)
         
-        # Generate a 10-character alphanumeric UUID
-        UUIDImage = str(uuid.uuid4())
+        # Generate a UUID (if 10-character alphanumeric is needed, this should be changed)
+        UUIDImage = str(uuid.uuid4())  # This generates a standard UUID
         
         # Process the image and save as PNG
         image_data = process_image(input_path, image_directory, UUIDImage + '.png')
-        # Process the image as jpg if needed
-        #image_data = process_image(input_path, image_directory, UUIDImage + '.jpg')
         
         # Update manifest
         manifest_data["images"].append({
@@ -49,8 +48,9 @@ for image_filename in os.listdir(raw_directory):
             "ScoreCraft": 0
         })
         
-        # Move the original image to completed
+        # Move the original image to completed_directory
         shutil.move(input_path, os.path.join(completed_directory, image_filename))
+
 # Save the updated manifest
 with open(manifest_path, 'w') as f:
     json.dump(manifest_data, f, indent=4)
