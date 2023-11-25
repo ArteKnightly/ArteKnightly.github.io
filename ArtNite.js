26c7921117c284fe6c06f8a5385bd44294733ed9
ArteKnightly <ArteKnightley@gmail.com>
2023-11-24 20:48:21 -0500

    up and running again

--- ArtNite.js
+++ ArtNite.js
@@ -4,16 +4,10 @@ let h1 = 32, h2 = 24, spaceBetween = 25;
 let paddingLeft, paddingRight, paddingTop, paddingBottom, messageHeight;
 let latestEvent, spotifyLink, spotifyEmbed;
 let isSetupComplete = false;
-function setup() {
-    console.log("Setup started");
-    createCanvas(windowWidth, windowHeight);
-    updateDynamicStyles();
-    preload();
-    calculatePadding();
-    background(255);
-    isSetupComplete = true;
-}
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    }
 function preload() {
     console.log("Preloading JSON data");
     jsonData = loadJSON('data/artNite.json', onJsonLoaded);
@@ -21,38 +15,85 @@ function preload() {
 
 function onJsonLoaded() {
     console.log('JSON successfully loaded:', jsonData);
+
+    // Calculate padding first since other elements depend on it
+    calculatePadding();
+    console.log('Padding calculation complete');
+
+    // Update styles based on the screen width
+    updateDynamicStyles();
+    console.log('Styles Update complete');
+
+    // Find the nearest future event index and update the latest event
     currentEventIndex = findNearestFutureEventIndex();
     updateLatestEvent();
-    setupSlider();  // Now calling setupSlider here
-    redrawEventDetails(); // and redrawEventDetails here
-}
+    console.log('Event update complete');
 
+    // Setup the slider
+    setupSlider();
+    console.log('Slider setup complete');
+
+    // Update Spotify player and links
+    updateSpotifyPlayer();
+    console.log('Spotify player update complete');
+    updateSpotifyLinks();
+    console.log('Spotify link update complete');
+
+    // Indicate that the initial setup is complete
+    isSetupComplete = true;
+    console.log('Initial setup complete');
+    isJsonLoaded = true;
+    console.log('isJsonLoaded setup complete');
+  }
 function setupSlider() {
     console.log("Setting up the slider");
+
     if (jsonData && jsonData.artNite) {
         eventSlider = createSlider(0, jsonData.artNite.length - 1, currentEventIndex, 1);
+        console.log("Slider created");
+
         adjustSliderForScreenSize();
+        console.log("Slider adjusted for screen size");
+
+        // Debugging logs
+        console.log("paddingTop:", paddingTop);
+        console.log("eventSlider height:", eventSlider.height);
+        console.log("eventSlider Width:", eventSlider.width);
+
+        // Calculate a suitable top position for the slider
+        let sliderY = paddingTop/2; // Adjust as needed
+        console.log("sliderY:", sliderY);
+
+        // Set the slider's position
         let sliderX = (windowWidth - eventSlider.width) / 2;
-        eventSlider.position(sliderX, paddingTop / 2);
+        console.log("sliderX:", sliderX);
+
+        eventSlider.position(sliderX, sliderY);
         eventSlider.changed(onSliderRelease);
     } else {
         console.log("jsonData or jsonData.artNite is not defined");
     }
 }
 function updateDynamicStyles() {
+    console.log("updateDynamicStyles begin")
     let screenWidth = windowWidth;
+    console.log("ScreenWidth: ", screenWidth )
 
     // Adjust font sizes based on screen width
-    h1 = screenWidth * 0.04; // 4% of the screen width
-    h2 = screenWidth * 0.03; // 3% of the screen width
-
+    h1 = screenWidth * 0.025; // 4% of the screen width
+    console.log("h1: ", h1)
+    h2 = screenWidth * 0.015; // 3% of the screen width
+    console.log("h2: ", h2)
     // Adjust spaceBetween dynamically
     spaceBetween = screenWidth * 0.025; // 2.5% of the screen width
-
+    console.log("spaceBetween: ", spaceBetween)
     // Ensure minimum sizes
     h1 = max(h1, 24); // Minimum size for h1
+    console.log("h1: ", h1)
     h2 = max(h2, 18); // Minimum size for h2
-    spaceBetween = max(spaceBetween, 15); // Minimum space
+    console.log("h2: ", h2)
+    spaceBetween = max(spaceBetween, 8); // Minimum space
+    console.log("spaceBetween: ", spaceBetween)
 }
 function adjustSliderForScreenSize() {
     if (eventSlider) { // Check if eventSlider is defined
@@ -61,32 +102,23 @@ function adjustSliderForScreenSize() {
 }
 
 function onSliderRelease() {
-    currentEventIndex = eventSlider.value();
-    updateLatestEvent();
-    background(0);
-    redrawEventDetails();
-    updateSpotifyLinks();
-}
-function onSliderRelease() {
     console.log("Slider released at value:", eventSlider.value());
     currentEventIndex = eventSlider.value();
     updateLatestEvent();
-    background(0);
+    console.log("event updated to:", latestEvent.Date);
+    background(255);
     redrawEventDetails();
     updateSpotifyLinks();
+    updateSpotifyPlayer(); // Update the Spotify player based on the current event
 }
-
-
 function redrawEventDetails() {
     if (!isSetupComplete) {
         return; // Exit the function if setup isn't complete
     }
-
     background(255);
     displayLatestEventDetails();
-    displayLatestEventDetailsFixed();
-   
-}
+    //displayLatestEventDetailsFixed();
+   }
 
 function displayLatestEventDetails() {
     textAlign(CENTER, CENTER);
@@ -120,14 +152,17 @@ function formatDate(isoDateString) {
 
 function calculatePadding() {
     // Define a uniform padding percentage for all sides
-    let paddingPercent = 0.1; // Example: 10% of the window's dimensions
-
+    let sidePaddingPercent = 0.04;
+    let topPaddingPercent = .06;
     // Apply this percentage to calculate padding for all sides
-    paddingLeft = windowWidth * paddingPercent;
+    paddingLeft = windowWidth * sidePaddingPercent;
+    console.log("paddingLeft calculated:", paddingLeft);
     paddingRight = paddingLeft; // Same as paddingLeft
-    paddingTop = windowHeight * paddingPercent;
-    paddingBottom = paddingTop; // Same as paddingTop
-
+    console.log("paddingRight calculated:", paddingRight);
+    paddingTop = windowHeight * topPaddingPercent;
+    console.log("paddingTop calculated:", paddingTop);
+    paddingBottom = windowHeight * topPaddingPercent*1.5; // Same as paddingTop
+    console.log("paddingBottom calculated:", paddingBottom);
     // Recalculate messageHeight if needed
     messageHeight = windowHeight - (paddingTop + paddingBottom);
 }
@@ -172,12 +207,16 @@ function drawCell(x, y, xOffset, yOffset, gridWidth, gridHeight) {
     //todo
 }
 function draw() {
+    if (isJsonLoaded) {
+        redrawEventDetails();
+        console.log("redrawEventDetails initial call");
+        isJsonLoaded = false; // Prevent continuous redrawing, remove if continuous drawing is needed
+    }
     if (mouseIsPressed) {
         stroke(0);
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
 }
-
 
 function windowResized() {
     console.log("Window resized");
@@ -186,9 +225,10 @@ function windowResized() {
     adjustSliderForScreenSize();
     calculatePadding();
     redrawEventDetails();
+
 }
 
-function createSpotifyEmbed(playlistId) {
+function createSpotifyEmbed(playlistId, playerWidth, playerHeight) {
     let embedUrl = `https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator`;
     let iframe = document.createElement('iframe');
 
@@ -198,8 +238,8 @@ function createSpotifyEmbed(playlistId) {
     iframe.style.boxShadow = 'none'; // No shadow for a flat design
 
     // Size adjustments (if needed)
-    iframe.width = '100%';
-    iframe.height = '300'; // Adjust height as per your design
+    iframe.width = playerWidth;
+    iframe.height = playerHeight; // Adjust height as per your design
 
     // Other necessary attributes
     iframe.frameBorder = '0';
@@ -215,10 +255,23 @@ function updateSpotifyPlayer() {
     if (spotifyEmbed) {
         spotifyEmbed.remove();
     }
-    spotifyEmbed = createSpotifyEmbed(playlistId);
+
+    // Calculate the width and position
+    let playerWidth = windowWidth * .5;
+    console.log("Player Width:", playerWidth)
+    let playerHeight = windowHeight * .25; // Or make this dynamic as per your design
+    console.log("Player Height:", playerHeight)
+    let playerX = windowWidth / 2 - playerWidth / 2;
+    console.log("Player X:", playerX)
+    let playerY = windowHeight-paddingBottom-playerHeight; // You can adjust this based on where you want it vertically
+    console.log("Player Y:", playerY)
+    spotifyEmbed = createSpotifyEmbed(playlistId, playerWidth, playerHeight);
+    spotifyEmbed.style.position = 'absolute';
+    spotifyEmbed.style.left = `${playerX}px`;
+    spotifyEmbed.style.top = `${playerY}px`;
+
     document.body.appendChild(spotifyEmbed);
 }
-
 function updateSpotifyLinks() {
     let today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -234,7 +287,8 @@ function updateSpotifyLinks() {
     } else {
         spotifyLink = createA(listeningLink, 'Listen on Spotify', '_blank');
     }
-    spotifyLink.position(width / 2 - spotifyLink.width / 2, height - (paddingBottom - spaceBetween));
+    spotifyLink.position(windowWidth / 2 - spotifyLink.width / 2, windowHeight - (paddingBottom / 2));
+    console.log("Spotify link drawn: X", windowWidth / 2 - spotifyLink.width / 2, "Y", windowHeight - (paddingBottom / 2) )
 }
 
 function isSameDay(d1, d2) {
@@ -260,15 +314,8 @@ function findNearestFutureEventIndex() {
     return jsonData.artNite.length - 1; // If no future events, return the last event
 }
 
-function onSliderChange() {
-    currentEventIndex = eventSlider.value();
-    getLatestEvent();
-    background(0);
-    redrawEventDetails();
-}
-
 function doubleClicked() {
     background(255);
-    drawGridFrame();
+    //drawGridFrame();
     displayLatestEventDetails();
 }
